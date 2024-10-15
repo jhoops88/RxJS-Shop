f4b1beeb72b7455b6cee62cca51a91ec3031928b
jacca <jacca@DESKTOP-RDVIJKS>
2024-10-15 21:32:27 +0100

    Added search field/bar which also uses a reactive programming style via the product service

--- rxjs-shop/src/app/product-list/product-list.component.ts
+++ rxjs-shop/src/app/product-list/product-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Product } from '../models/product.model';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [AsyncPipe, ProductDetailComponent, MatSelectModule],
+  imports: [CommonModule, AsyncPipe, ProductDetailComponent, MatSelectModule, MatInputModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
@@ -26,9 +27,14 @@ export class ProductListComponent implements OnInit {
     ngOnInit(): void {
     this.products$ = this.productService.product$;
     this.categories$ = this.productService.productCategories$;
+    // this.productService.searchProducts('laptop');
   }
 
   onCategorySelect(selectedCategory: string) {
     this.productService.selectedCategoryChanged(selectedCategory);
-}
+  }
+
+  searchProducts(searchTerm: string): void {
+    this.productService.searchProducts(searchTerm); // Update the search term
+  }
 }
--- rxjs-shop/src/app/product.service.ts
+++ rxjs-shop/src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, distinct, filter, from, map, mergeMap, of, switchMap, toArray } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, combineLatest, debounceTime, distinct, filter, from, map, mergeMap, of, switchMap, toArray } from 'rxjs';
 import { ProductApiStub } from './stubs/product-api-stub';
 import { Product } from './models/product.model';
 
@@ -10,6 +10,20 @@ export class ProductService {
 
   private allProduct$: Observable<Product[]> = of(ProductApiStub.dummyProducts);
   
+  private searchSubject = new BehaviorSubject<string>('');
+  searchAction$ = this.searchSubject.asObservable();
+
+  productSearch$: Observable<Product[]> = this.searchAction$.pipe(
+    debounceTime(300),
+    switchMap(searchTerm => 
+      this.allProduct$.pipe(
+        mergeMap(products => of(...products)),
+        filter(product => product.name.includes(searchTerm) || product.description.includes(searchTerm)),
+        toArray()
+      )
+    )
+  );
+
   productCategories$: Observable<string[]> = this.allProduct$.pipe(
     mergeMap(products => of(...products)),
     distinct(product => product.category),
@@ -20,11 +34,17 @@ export class ProductService {
   private categorySubject = new BehaviorSubject<string|null>(null);
   categorySelectAction$ = this.categorySubject.asObservable();
 
-  product$: Observable<Product[]> = this.categorySelectAction$.pipe(
-    switchMap(catName => 
+  product$: Observable<Product[]> = combineLatest([
+    this.categorySelectAction$,
+    this.searchAction$
+  ]).pipe(
+    switchMap(([catName, searchTerm]) => 
       this.allProduct$.pipe(
         mergeMap(products => of(...products)),
-        filter(product => catName === null || product.category === catName),
+        filter(product => 
+          (catName === null || product.category === catName) &&
+          (searchTerm === '' || product.name.includes(searchTerm) || product.description.includes(searchTerm))
+        ),
         toArray()
       )
     )
@@ -36,6 +56,8 @@ export class ProductService {
     this.categorySubject.next(categoryName);
   }
 
+  searchProducts(searchTerm: string): void {
+    this.searchSubject.next(searchTerm);
+  }
 
-  // cats$ = this.categorySelectAction$.pipe(map(catName => this.product$.pipe(from(ProductApiStub.getForCategory(catName)))))
 }
